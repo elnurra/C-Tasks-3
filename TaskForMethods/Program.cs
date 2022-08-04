95f37664bf6bf691b96511ff8fd5fdcc3420a008
elnurra <elnurra@code.edu.az>
2022-08-04 15:30:31 +0400

    Homework task successfully completed

---
+++ TaskForMethods/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace TaskForMethods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             int[] arr = { 54, 56, 76, 17, 45, 756, 87 };
+            Console.WriteLine( Min(arr));
+        }
+        static int Min( int[] arr ) 
+        {
+            int min,index=0;
+            
+            min = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (min > arr[i]) 
+                {
+                    min = arr[i];
+                    index=i;
+                }
+
+            }
+            Console.Write( "The minimum element of array in index " + index + " equals to " );
+            return min;
+        }
+    }
+}
---
+++ TaskForSquaresOfFigures/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace TaskForSquaresOfFigures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value for variable a: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value for variable b: ");
+            int b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value for variable c: ");
+            int c = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a value for variable r(radius): ");
+            int r = int.Parse(Console.ReadLine());
+            Console.WriteLine("Overload 1: The area of a circle: ");
+            Console.WriteLine(Area(r));
+            Console.WriteLine("Overload 2: The area of a rectangle ");
+            Console.WriteLine(Area(a,b));
+            Console.WriteLine("Overload 3: The area of a rectangular parallelepiped: ");
+            Console.WriteLine(Area(a, b, c));
+            Console.WriteLine("Overload 4: The area of a circle of an inscribed triangle: ");
+            Console.WriteLine(Area(a, b, c, r));
+        }
+       static int Area(int r) 
+        {
+            int pi = 3;
+            int S = pi * r * r;
+            return S;
+        }
+        static int Area(int a, int b)
+        {
+
+            int S = a * b;
+            return S;
+        }
+        static int Area(int a, int b,int c)
+        {
+
+            int S = 2*((a*b)+(a*c)+(b*c));
+            return S;
+        }
+        static int Area(int a, int b, int c, int r)
+        {
+            int halfper = ((a + b + c) / 2);
+            int S = halfper * r;
+            return S;
+        }
+    }
+}
